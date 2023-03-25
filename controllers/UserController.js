15497e7f0d755b32d2e6c9361d0a53e9793984ea
Jakub <sorok028@umn.edu>
2023-03-25 12:21:40 -0500

    createUser and finduserbyid

---
+++ controllers/UserController.js
@@ -0,0 +1,24 @@
+const { User, Location, Run } = require('../models')
+
+const finUserById = async (req, res, next, id) => {
+    try {
+        const user = await User.findByPk(id)
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' })
+        }
+        req.user = user
+        next()
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+const createUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body
+        const user = await User.create({ username, email, password })
+        res.status(201).json(user)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+}
