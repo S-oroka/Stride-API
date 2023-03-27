e049a8b2a9358703cac65b7eba0919c29ad43499
Jakub <sorok028@umn.edu>
2023-03-27 10:16:44 -0500

    auth working

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -2,7 +2,7 @@ const Router = require('express').Router()
 const controller = require('../controllers/AuthController')
 // const middleware = require('../middleware')
 
-Router.post('./register', controller.Register)
+Router.post('/register', controller.Register)
 // Router.get('/session', 
 // middleware.stripToken,
 // middleware.verifyToken,
